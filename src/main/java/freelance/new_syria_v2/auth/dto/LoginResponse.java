615aa94e046db338248cd744ee795618f379b82e
MostafaMahmoudegy10 <mostafa.mahmoudegy10@gmail.com>
2025-10-08 22:32:27 +0300

    login response

--- src/main/java/freelance/new_syria_v2/auth/dto/LoginResponse.java
+++ src/main/java/freelance/new_syria_v2/auth/dto/LoginResponse.java
@@ -2,6 +2,7 @@ package freelance.new_syria_v2.auth.dto;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.security.core.GrantedAuthority;
 
@@ -15,6 +16,8 @@ import lombok.NoArgsConstructor;
 public class LoginResponse {
     private String token;
     private List<String> roles;
+    private UUID userId;
+    private String email;
 
     public static LoginResponse from(String token, Collection<? extends GrantedAuthority> authorities) {
         List<String> roles = authorities.stream()
@@ -22,5 +25,12 @@ public class LoginResponse {
                                         .toList();
         return new LoginResponse(token, roles);
     }
+
+	public LoginResponse(String token, List<String> roles) {
+		super();
+		this.token = token;
+		this.roles = roles;
+	}
+    
 }
 
--- src/main/java/freelance/new_syria_v2/auth/service/LoginService.java
+++ src/main/java/freelance/new_syria_v2/auth/service/LoginService.java
@@ -1,6 +1,5 @@
 package freelance.new_syria_v2.auth.service;
 
-
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -21,36 +20,39 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class LoginService {
-    private final CustomUserDetailsService userService;
-    private final PasswordEncoder encoder;
-    private final TokenService tokenService;
+	private final CustomUserDetailsService userService;
+	private final PasswordEncoder encoder;
+	private final TokenService tokenService;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoginService.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginService.class);
 
-    public LoginResponse login(LoginDto dto) {
+	public LoginResponse login(LoginDto dto) {
 
-    	boolean present = this.userService.isPresent(dto.getEmail());
-    	if(present) {
-    		UserDetails userDetails = this.userService.loadUserByUsername(dto.getEmail());
-    		// check password
-	        if (!encoder.matches(dto.getPassword(), userDetails.getPassword())) {
-	            LOGGER.warn("Invalid login attempt for email: {}", dto.getEmail());
-	            throw new BadCredentialsException("Invalid email or password");
-	        }
-	
-	        // check account status
-	        if (!userDetails.isEnabled()) {
-	            LOGGER.error("User {} tried to login but is not enabled. Check your email.", userDetails.getUsername());
-	            throw new BadCredentialsException("Your account is not enabled. Please check your email.");
-	        }
-	
-	      Optional<Token> token = this.tokenService.findLatestTokenByEmail(dto.getEmail());	  
-	      if(token.isEmpty()) {
-	    	 User user= this.userService.findOptionalByEmail(userDetails.getUsername());
-	    	  this.tokenService.save(user);
-	      }
-	      return new LoginResponse().from(token.get().getToken(), userDetails.getAuthorities());
-    	}
-    	throw new NotFoundException("email you try to login with is not present");
-    }
+		boolean present = this.userService.isPresent(dto.getEmail());
+		if (present) {
+			CustomUserDetails userDetails = (CustomUserDetails) this.userService.loadUserByUsername(dto.getEmail());
+			// check password
+			if (!encoder.matches(dto.getPassword(), userDetails.getPassword())) {
+				LOGGER.warn("Invalid login attempt for email: {}", dto.getEmail());
+				throw new BadCredentialsException("Invalid email or password");
+			}
+
+			// check account status
+			if (!userDetails.isEnabled()) {
+				LOGGER.error("User {} tried to login but is not enabled. Check your email.", userDetails.getUsername());
+				throw new BadCredentialsException("Your account is not enabled. Please check your email.");
+			}
+
+			Optional<Token> token = this.tokenService.findLatestTokenByEmail(dto.getEmail());
+			if (token.isEmpty()) {
+				User user = this.userService.findOptionalByEmail(userDetails.getUsername());
+				this.tokenService.save(user);
+			}
+			var res = new LoginResponse().from(token.get().getToken(), userDetails.getAuthorities());
+			res.setUserId(userDetails.getUser().getId());
+			res.setEmail(userDetails.getUser().getEmail());
+			return res;
+		}
+		throw new NotFoundException("email you try to login with is not present");
+	}
 }
